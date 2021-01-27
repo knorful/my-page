8730b927ad77bf28523b04d8190fb7ff291eaef2
knorful <krisnorful@gmail.com>
2021-01-27 12:44:47 -0600

    add onScroll effect for About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 
 import classes from "./About.module.css";
+import styled from "styled-components";
 
-const about = () => {
+const About = () => {
+  const [show, doShow] = useState({
+    itemOne: false,
+    itemTwo: false,
+  });
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+
+  useLayoutEffect(() => {
+    const topPosition = (element) => element.getBoundingClientRect().top;
+
+    const div1Pos = topPosition(ref1.current);
+
+    const onScroll = () => {
+      const scrollPosition = window.scrollY + window.innerHeight;
+      if (div1Pos < scrollPosition) {
+        doShow((state) => ({ ...state, itemOne: true, itemTwo: true }));
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <div className={classes.About}>
       <h1>About</h1>
       <hr />
       <div className={classes.bio}>
-        <div className={classes.background}>
+        <Div animate={show.itemOne} ref={ref1} className={classes.background}>
           <p>
             Contrary to popular belief, Lorem Ipsum is not simply random text.
             It has roots in a piece of classical Latin literature from 45 BC,
@@ -23,8 +47,8 @@ const about = () => {
             Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
             amet..", comes from a line in section 1.10.32.
           </p>
-        </div>
-        <div className={classes.skills}>
+        </Div>
+        <Div2 animate={show.itemTwo} ref={ref2} className={classes.skills}>
           <h2>Skills: </h2>
           <ul>
             <li>JavaScript</li>
@@ -33,10 +57,19 @@ const about = () => {
             <li>React</li>
             <li>Redux</li>
           </ul>
-        </div>
+        </Div2>
       </div>
     </div>
   );
 };
 
-export default about;
+const Div = styled.div`
+  transform: translateX(${({ animate }) => (animate ? "0" : "-100vw")});
+  transition: transform 1s;
+`;
+
+const Div2 = styled.div`
+  transform: translateX(${({ animate }) => (animate ? "0" : "100vw")});
+  transition: transform 1s;
+`;
+export default About;
