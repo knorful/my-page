aa117a051f94f0e53e292df6ae6b8f88de00ba1a
Kris Norful <krisnorful@gmail.com>
2021-04-20 17:57:19 -0500

    make show cards for portfolio projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,16 @@
 import React, { useState, useRef, useLayoutEffect } from "react";
-
+import RedditIcon from "@material-ui/icons/Reddit";
 import classes from "./Projects.module.css";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const Projects = () => {
   const [show, doShow] = useState({
     itemOne: false,
   });
+  const [hoverJammming, setHoverJammming] = useState(false);
+  const [hoverReddit, setHoverReddit] = useState(false);
+  const [hoverPortfolio, setHoverPortfolio] = useState(false);
+
   const ref1 = useRef(null);
 
   useLayoutEffect(() => {
@@ -29,29 +33,50 @@ const Projects = () => {
     <div id="projects" className={classes.Projects}>
       <h1>Projects</h1>
       <Div1 ref={ref1} animate={show.itemOne} className={classes.container}>
-        <div className={classes.box}>
-          <p>Project 1</p>
+        <div
+          className={classes.jammingBox}
+          onMouseEnter={() => setHoverJammming(true)}
+          onMouseLeave={() => setHoverJammming(false)}
+        >
+          <p>Jammming</p>
+          {hoverJammming ? (
+            <div>
+              <button>View Project</button>
+            </div>
+          ) : null}
         </div>
-        <div className={classes.box}>
-          <p>Project 2</p>
+
+        <div
+          className={classes.redditBox}
+          onMouseEnter={() => setHoverReddit(true)}
+          onMouseLeave={() => setHoverReddit(false)}
+        >
+          <RedditIcon className={classes.redditLogo} />
+          <h2>
+            Reddit <span>Minimal</span>
+          </h2>
+          {hoverReddit ? (
+            <div>
+              <button>View Project</button>
+            </div>
+          ) : null}
         </div>
-        <div className={classes.box}>
-          <p>Project 3</p>
+        <div
+          className={classes.portfolioBox}
+          onMouseEnter={() => setHoverPortfolio(true)}
+          onMouseLeave={() => setHoverPortfolio(false)}
+        >
+          <p>Portfolio</p>
+          {hoverPortfolio ? (
+            <div>
+              <button>View Project</button>
+            </div>
+          ) : null}
         </div>
       </Div1>
     </div>
   );
 };
-
-const opacity = keyframes`
-  from {
-    opacity: 0;
-  }
-
-  to {
-    opacity: 1;
-  }
-`;
 
 const Div1 = styled.div`
   transform: translateX(${({ animate }) => (animate ? "0" : "100vw")});
